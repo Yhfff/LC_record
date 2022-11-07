78e460a862590512ea5bd0f8e00874d2c5d6ef9c
YHF <yuhongfu0603@163.com>
2022-11-07 19:07:21 +0800

    push commit

--- src/Greedy/LC_435.java
+++ src/Greedy/LC_435.java
@@ -3,6 +3,10 @@ package Greedy;
 //无重叠区间
 public class LC_435 {
 
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+
     public int eraseOverlapIntervals(int[][] intervals) {
         return 1;
     }
